ee5d18111a5ba7f902b532d83b92050057669809
sumitradas94 <sumitradas94@gmail.com>
2023-01-02 01:48:16 +0530

    Perfectnumber

---
+++ PerfectNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logial_Problem
+{
+    public class PerfectNumber
+    {
+        public static void checkperfectno()
+        {
+            int sum = 0;
+            Console.WriteLine("Enter the number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    sum = sum + i;
+                    Console.WriteLine("factor of num is:" + sum);
+                }
+            }
+            if (sum == num)
+            {
+                Console.WriteLine("num is perfect number");
+            }
+            else
+            {
+                Console.WriteLine("num is not a perfect number");
+            }
+        }
+    }
+}
