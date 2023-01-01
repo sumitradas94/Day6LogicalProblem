c871e31396f4e12cc1870af652262440394d554e
sumitradas94 <sumitradas94@gmail.com>
2023-01-02 01:53:11 +0530

    checking the reverse no

---
+++ ReverseNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logial_Problem
+{
+    public class ReverseNumber
+    {
+        public static void checkreverseno()
+        {
+            int Reverse = 0;
+            Console.WriteLine("Enter the number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                int Remainder = num % 10;
+                Reverse = Reverse * 10 + Remainder;
+                num = num / 10;
+            }
+            Console.WriteLine(Reverse);
+        }
+    }
+}
