6c38624070c9d5b708adcb9c02484bf9ff1a9a74
sumitradas94 <sumitradas94@gmail.com>
2023-01-03 22:07:22 +0530

    calculating interval time

---
+++ StopWatch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logial_Problem
+{
+    public class StopWatch
+    {
+        public static void Stop()
+        {
+            Console.WriteLine("\n***************Stop Watch****************\n ");
+            Console.WriteLine("Press Enter to Start.");
+            Console.ReadLine();
+            DateTime Start = DateTime.Now;
+
+            Console.WriteLine("Press Enter to Stop.");
+            Console.ReadLine();
+            DateTime Stop = DateTime.Now;
+
+            Console.Write("Elapsed Time = " + (Stop - Start));
+        }
+    }
+}
